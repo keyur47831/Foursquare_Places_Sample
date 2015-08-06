0bc17902c48d6ea67fdfdf7899cfa5b10816e4ed
keyur47831 <keyur.thakkar@hotmail.com>
2015-08-06 23:58:35 +1000

    initial_changes

---
+++ TourGuide/app/src/main/java/com/sample/tourguide/Define.java
@@ -0,0 +1,8 @@
+package com.sample.tourguide;
+
+/**
+ * Created by keyur on 06-08-2015.
+ */
+public class Define {
+    public static String GOOGLE_KEY="AIzaSyAY-pIjIay2nShWwyCF0QkjwY92d_3tvss";
+}
