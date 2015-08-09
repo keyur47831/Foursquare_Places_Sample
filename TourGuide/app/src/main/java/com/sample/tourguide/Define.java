e5a5b57066b03989c74e3fa33f73b7cd23dd6982
keyur47831 <keyur.thakkar@hotmail.com>
2015-08-09 22:09:51 +1000

    spellcheck

--- TourGuide/app/src/main/java/com/sample/tourguide/Define.java
+++ TourGuide/app/src/main/java/com/sample/tourguide/Define.java
@@ -10,7 +10,7 @@ import android.net.ConnectivityManager;
 public class Define {
     //public static String GOOGLE_KEY="AIzaSyAY-pIjIay2nShWwyCF0QkjwY92d_3tvss";
     public static String CLIENT_ID="XWLGRC03I0EFFEFCSIKSRTDVN5IK5Y1JMBR4DOWOAK2TC25C";
-    public static String CLINED_SECRET="K1QFNQVG0JSK22B0ASDZXRTDT2FEVBTTQS2KSESFRWVW1153";
+    public static String CLIENT_SECRET="K1QFNQVG0JSK22B0ASDZXRTDT2FEVBTTQS2KSESFRWVW1153";
     public static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
 
     // The minimum time between updates in milliseconds
@@ -19,5 +19,5 @@ public class Define {
     public static enum ErrorStatus {
         No_GPS, No_Internet, Parse_Error
     }
-    public static String FOUR_SQUARE_URL="https://api.foursquare.com/v2/venues/search?client_id="+Define.CLIENT_ID+"&client_secret="+Define.CLINED_SECRET+"&v=20150711&limit=100"+"&query=tourist%20attraction";
+    public static String FOUR_SQUARE_URL="https://api.foursquare.com/v2/venues/search?client_id="+Define.CLIENT_ID+"&client_secret="+Define.CLIENT_SECRET+"&v=20150711&limit=100"+"&query=tourist%20attraction";
 }
